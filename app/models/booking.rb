d68e4410db42ba8a3ceb812006625d4a729c77dd
Davis Namsons <davisnamsons@gmail.com>
2022-12-29 16:01:50 +0200

    Add validation to avoid overlapping bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,24 @@
 class Booking < ApplicationRecord
+  validate :booking_times_cannot_overlap
+
   def self.in_date_range(date_range)
     datetime_range = (date_range.first.beginning_of_day..date_range.last.end_of_day)
 
     where(start_time: datetime_range).or(where(end_time: datetime_range))
+  end
+
+  def self.overlaps_exclusively_with(column_name, range)
+    where("#{column_name} > :start AND #{column_name} < :end", start: range.first, end: range.last)
+  end
+
+  # Finds bookings that overlap with a booking.
+  # This is an exclusive check - the start and end of the range is ignored
+  # We do this because it is perfectly okay for a one booking to end on the midnight
+  # And for the next booking to start on the midnight
+  def self.overlapping(booking)
+    booking_range = booking.start_time..booking.end_time
+
+    overlaps_exclusively_with(:start_time, booking_range).or(overlaps_exclusively_with(:end_time, booking_range))
   end
 
   def starts_on_midnight?
@@ -11,5 +27,11 @@ class Booking < ApplicationRecord
 
   def crosses_midnight?
     start_time.to_date.tomorrow == end_time.to_date
+  end
+
+  def booking_times_cannot_overlap
+    return if Booking.overlapping(self).none?
+
+    errors.add(:base, "There is an already booked time slot with overlapping time")
   end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Booking do
+  let(:midnight) { DateTime.current.beginning_of_day }
+
   describe '.in_date_range' do
-    subject { described_class.in_date_range(Date.current.beginning_of_month..Date.current.end_of_month) }
+    subject { described_class.in_date_range(beginning_of_month..end_of_month) }
+
+    let(:beginning_of_month) { DateTime.current.beginning_of_month }
+    let(:end_of_month) { DateTime.current.end_of_month }
+    let(:next_month) { DateTime.current.next_month.beginning_of_month }
 
     let!(:booking_ending_at_the_beginning_of_month) do
-      create :booking, start_time: DateTime.current.last_month.end_of_month, end_time: DateTime.current.beginning_of_month.end_of_hour
+      create :booking, start_time: DateTime.current.last_month.end_of_month, end_time: beginning_of_month.end_of_hour
     end
     let!(:booking_starting_at_the_end_of_month) do
-      create :booking, start_time: DateTime.current.end_of_month.beginning_of_hour, end_time: DateTime.current.next_month.beginning_of_month
+      create :booking, start_time: end_of_month.beginning_of_hour, end_time: next_month
     end
     let!(:booking_starting_on_the_next_month) do
-      create :booking, start_time: DateTime.current.next_month, end_time: DateTime.current.next_month.end_of_hour
+      create :booking, start_time: next_month, end_time: next_month + 1.hour
     end
 
     it 'finds bookings overlapping with the range' do
@@ -22,17 +28,39 @@ RSpec.describe Booking do
     end
   end
 
+  describe 'booking_times_cannot_overlap validation' do
+    subject { booking.valid? }
+
+    let!(:booking_from_midnight) { create :booking, start_time: midnight, end_time: midnight + 1.hour }
+
+    let(:booking) { build :booking, start_time: midnight + 3.hours, end_time: midnight + 4.hours }
+
+    it { is_expected.to eq(true) }
+
+    context 'with the booking starting yesterday and ending today' do
+      let(:booking) { build :booking, start_time: midnight - 1.hour, end_time: midnight + 30.minutes }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'with the booking starting on the midnight' do
+      let(:booking) { build :booking, start_time: midnight, end_time: midnight + 30.minutes }
+
+      it { is_expected.to eq(true) }
+    end
+  end
+
   describe '#starts_on_midnight?' do
     subject { booking.starts_on_midnight? }
 
     let(:booking) do
-      build :booking, start_time: DateTime.current.change(hour: 1), end_time: DateTime.current.change(hour: 2)
+      build :booking, start_time: midnight + 1.hour, end_time: midnight + 2.hours
     end
 
     it { is_expected.to eq(false) }
 
     context 'with a booking starting on midnight' do
-      let(:booking) { build :booking, start_time: Date.current.to_datetime, end_time: DateTime.current.change(hour: 2) }
+      let(:booking) { build :booking, start_time: midnight, end_time: midnight + 2.hours }
 
       it { is_expected.to eq(true) }
     end
@@ -41,12 +69,12 @@ RSpec.describe Booking do
   describe '#crosses_midnight?' do
     subject { booking.crosses_midnight? }
 
-    let(:booking) { build :booking, start_time: DateTime.current.change(hour: 1), end_time: DateTime.current.change(hour: 2)}
+    let(:booking) { build :booking, start_time: midnight + 1.hour, end_time: midnight + 2.hours }
 
     it { is_expected.to eq(false) }
 
     context 'with a booking starting yesterday and ending today' do
-      let(:booking) { build :booking, start_time: DateTime.yesterday.end_of_day, end_time: DateTime.current.change(hour: 2)}
+      let(:booking) { build :booking, start_time: midnight - 1.hour, end_time: midnight + 2.hours }
 
       it { is_expected.to eq(true) }
     end
